7407494abb44aed423fbf479022d85cba57a6cd6
MSI\s454666 <s454666123@gmail.com>
2024-04-20 20:00:53 +0800

    excel

--- app/Exports/BuildingAddressExport.php
+++ app/Exports/BuildingAddressExport.php
@@ -21,7 +21,7 @@ class BuildingAddressExport
         $sheet       = $spreadsheet->getActiveSheet();
 
         // 假設原始文本是 Big5 編碼，需要轉換為 UTF-8
-        $utf8Text = mb_convert_encoding($this->text, 'UTF-8', 'Big5');
+        $utf8Text = $this->text;
         file_put_contents('utf8_text_output.txt', $utf8Text);
         // 搜索 "建物門牌:" 開始到文本結尾的任何字符，包括中文和換行符
         $pattern = '/建物門牌(.*)/us';  // 使用 u 修飾符支持 UTF-8，使用 s 使 . 匹配包括換行符在內的任何字符

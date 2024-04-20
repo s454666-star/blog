aabf6771b1904f3436367cc4d4525ea6127a3093
MSI\s454666 <s454666123@gmail.com>
2024-04-20 19:09:29 +0800

    pdf2

--- app/Http/Controllers/PdfController2.php
+++ app/Http/Controllers/PdfController2.php
@@ -3,15 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Symfony\Component\Process\Exception\ProcessFailedException;
-use Symfony\Component\Process\Process;
+use Illuminate\Support\Facades\Storage;
 
 class PdfController2 extends Controller
 {
     // 顯示上傳表單
     public function showUploadForm()
     {
-        return view('pdf.upload2');
+        return view('pdf.upload2');  // 指向 resources/views/pdf/upload2.blade.php
     }
 
     // 處理PDF文件上傳並提取文字
@@ -24,24 +23,24 @@ class PdfController2 extends Controller
         $pdfFile = $request->file('pdf');
         $pdfPath = $pdfFile->path();
 
-        // 使用 pdftotext 轉換 PDF 文本
+        // 使用外部命令處理 PDF
         $process = new Process(['pdftotext', '-layout', $pdfPath, '-']);
         $process->run();
 
-        // 執行過程中發生錯誤
+        // 檢查命令執行是否成功
         if (!$process->isSuccessful()) {
             throw new ProcessFailedException($process);
         }
 
-        $output = $process->getOutput();
+        // 保存文本到文件
+        Storage::put('pdf_output.txt', $process->getOutput());
 
-        // 手動檢查換行符
-        $text = str_replace("\n", "\r\n", $output);
+        // 讀取文件內容
+        $text = Storage::get('pdf_output.txt');
 
-        // 調試輸出
-        file_put_contents('pdf_output.txt', $text); // 將處理後的文本保存至文件以進行檢查
+        // 替換換行符以確保格式
+        $text = str_replace("\n", "\r\n", $text);
 
-        return view('pdf.text', compact('text'));
+        return view('pdf.display', compact('text'));  // 確保有一個叫做 'pdf.display' 的視圖
     }
-
 }

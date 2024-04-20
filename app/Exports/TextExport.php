ed752dd8c2f436d49290ca2b21ceb98acf8efc50
MSI\s454666 <s454666123@gmail.com>
2024-04-20 19:34:41 +0800

    excel

--- app/Exports/TextExport.php
+++ app/Exports/TextExport.php
@@ -17,15 +17,15 @@ class TextExport implements FromCollection, WithHeadings
 
     public function collection()
     {
-        // 搜索 "建物門牌:" 到 "\r\n" 的文本
-        $pattern = '/建物門牌:(.*?)\r\n/';
+        // 搜索 "建物門牌:" 開始到文本結尾
+        $pattern = '/建物門牌:(.*)/';
         preg_match($pattern, $this->text, $matches);
 
-        $address = $matches[1] ?? '未找到';  // 如果沒有找到則返回 '未找到'
+        $address = trim($matches[1] ?? '未找到');  // 如果沒有找到則返回 '未找到'
 
         // 返回包含提取的建物門牌資訊的集合
         return new Collection([
-            ['content' => $address]
+            ['building_address' => $address]
         ]);
     }
 

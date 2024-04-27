b45609672913be15bfed816e2125af9793eafdca
MSI\s454666 <s454666123@gmail.com>
2024-04-27 11:49:17 +0800

    圖片功能

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -44,20 +44,37 @@ class GalleryController extends Controller
     protected function compressImage($sourcePath, $destinationPath)
     {
         list($width, $height) = getimagesize($sourcePath);
-        $newWidth  = 320;
+        $newWidth = 320;
         $newHeight = 240;
 
-        $thumb  = imagecreatetruecolor($newWidth, $newHeight);
+        $thumb = imagecreatetruecolor($newWidth, $newHeight);
         $source = imagecreatefromjpeg($sourcePath);
 
         imagecopyresized($thumb, $source, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+
         $destinationFullPath = public_path($destinationPath);
+
+        // Check if directory exists and is writable
+        $directory = dirname($destinationFullPath);
+        if (!file_exists($directory) && !mkdir($directory, 0775, true)) {
+            Log::error("Failed to create directory: " . $directory);
+            return false;
+        }
+
+        if (!is_writable($directory)) {
+            Log::error("Directory not writable: " . $directory);
+            return false;
+        }
+
         if (!imagejpeg($thumb, $destinationFullPath, 75)) {
+            Log::error("Failed to save image: " . $destinationFullPath);
             return false; // Return false if compression failed
         }
+
         imagedestroy($thumb); // Free up memory
         return $destinationPath;
     }
+
 
     public function show($filename)
     {

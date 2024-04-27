9ae6d2da7fae2d92495a36dde870b5366f986095
MSI\s454666 <s454666123@gmail.com>
2024-04-27 11:18:48 +0800

    圖片功能

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -6,16 +6,16 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
 use Symfony\Component\Finder\Finder;
 
-// 引入 Finder
-
 class GalleryController extends Controller
 {
     public function index()
     {
         $photoPath = config('gallery.photo_path');
         $finder    = new Finder();
-        $folders   = File::directories($photoPath);
-        $images    = collect();
+        $finder->files()->in($photoPath); // Correctly setting up the directory for Finder
+
+        $folders = File::directories($photoPath);
+        $images  = collect();
 
         foreach ($folders as $folder) {
             $files  = File::allFiles($folder);
@@ -23,8 +23,8 @@ class GalleryController extends Controller
         }
 
         $images = $images->random(150)->values()->all();
-        $finder->files()->in($photoPath); // 設置要搜索的目錄
 
         return view('gallery.index', compact('images', 'finder'));
     }
 }
+

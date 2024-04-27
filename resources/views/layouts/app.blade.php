888f282dfe1b7412e1e463615df327cd3fba45fe
MSI\s454666 <s454666123@gmail.com>
2024-04-27 16:51:51 +0800

    圖片問題

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    @yield('head')  <!-- 這裡加入 @yield 來引入子視圖的 head 部分 -->
+    @yield('head', '<!-- No extra head content -->')  <!-- 提供預設值 -->
 </head>
 <body>
 <div id="app">
     @yield('content')
 </div>
 
-@yield('scripts')  <!-- 建議把 scripts 也移到這裡來確保載入順序正確 -->
+@yield('scripts', '<!-- No scripts to load -->')  <!-- 提供預設值 -->
 </body>
 </html>

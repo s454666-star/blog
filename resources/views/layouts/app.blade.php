75e6a367f2f478d7b7df73c79d71f59c918a8ce6
MSI\s454666 <s454666123@gmail.com>
2024-04-27 20:33:09 +0800

    圖片問題

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    @yield('head', '<!-- No extra head content -->')  <!-- 提供預設值 -->
+    @yield('head', '')  <!-- 提供預設值 -->
 </head>
 <body>
 <div id="app">
     @yield('content')
 </div>
 
-@yield('scripts', '<!-- No scripts to load -->')  <!-- 提供預設值 -->
+@yield('scripts', '')  <!-- 提供預設值 -->
 </body>
 </html>

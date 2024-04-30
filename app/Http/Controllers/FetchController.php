a7f5278883c02319caa1df6878148298b1ab9d5b
MSI\s454666 <s454666123@gmail.com>
2024-04-30 20:55:17 +0800

    亞馬遜商品

--- app/Http/Controllers/FetchController.php
+++ app/Http/Controllers/FetchController.php
@@ -3,29 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
+use GuzzleHttp\Client;
 use App\Services\CrawlerProductService;
 
 class FetchController extends Controller
 {
     protected $crawler;
+    protected $client;
 
     public function __construct(CrawlerProductService $crawler)
     {
         $this->crawler = $crawler;
+        $this->client  = new Client(); // 初始化 GuzzleHttp Client
     }
 
     public function fetchData(Request $request): string
     {
         $url = $request->input('url');
+
         try {
-            $response           = Http::get($url);
-            $html               = $response->body();
+            // 发送请求，设置 User-Agent
+            $response = $this->client->request('GET', $url, [
+                'headers' => [
+                    'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+                ]
+            ]);
+            $html     = (string)$response->getBody();
+
             $productTitle       = $this->crawler->getProductTitle($html);
             $productPrice       = $this->crawler->getProductPrice($html);
             $productDescription = $this->crawler->getProductDescription($html);
             $packageWeight      = $this->crawler->getPackageWeight($html);
-            $formattedResponse  = "商品名稱: " . $productTitle . "\n商品價格: " . $productPrice . "\n商品說明: " . $productDescription . "\n梱包重量: " . ($packageWeight ?: "無資訊") . "\n\n".$html;
+
+            $formattedResponse = "商品名稱: " . $productTitle .
+                "\n商品價格: " . $productPrice .
+                "\n商品說明: " . $productDescription .
+                "\n梱包重量: " . ($packageWeight ?: "無資訊") . "\n\n" . $html;
             return $formattedResponse;
         }
         catch (\Exception $e) {

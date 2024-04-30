82457b38fa0d3d56f25f37971643ae1e281523b2
MSI\s454666 <s454666123@gmail.com>
2024-04-30 20:57:16 +0800

    亞馬遜商品

--- app/Http/Controllers/FetchController.php
+++ app/Http/Controllers/FetchController.php
@@ -25,7 +25,13 @@ class FetchController extends Controller
             // 发送请求，设置 User-Agent
             $response = $this->client->request('GET', $url, [
                 'headers' => [
-                    'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+                    'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
+                    'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                    'Accept-Language' => 'en-US,en;q=0.5',
+                    'Accept-Encoding' => 'gzip, deflate, br',
+                    'Connection' => 'keep-alive',
+                    'Upgrade-Insecure-Requests' => '1',
+                    'TE' => 'Trailers'
                 ]
             ]);
             $html     = (string)$response->getBody();

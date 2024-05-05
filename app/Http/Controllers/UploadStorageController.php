2c3a372417d65f722614e22be31c90bb91d8bb27
MSI\s454666 <s454666123@gmail.com>
2024-05-05 09:51:39 +0800

    上傳測試

--- app/Http/Controllers/UploadStorageController.php
+++ app/Http/Controllers/UploadStorageController.php
@@ -18,18 +18,8 @@ class UploadStorageController extends Controller
         $fileName = $file->getClientOriginalName();
 
         $storage = new StorageClient([
-            'keyFile' => [
-                'type' => 'service_account',
-                'project_id' => env('GOOGLE_CLOUD_PROJECT_ID'),
-                'private_key_id' => env('GOOGLE_CLOUD_HMAC_ACCESS_KEY'),
-                'private_key' => env('GOOGLE_CLOUD_HMAC_SECRET_KEY'),
-                'client_email' => 's4546664234@stellar-day-420712.iam.gserviceaccount.com',
-                'client_id' => '',
-                'auth_uri' => 'https://accounts.google.com/o/oauth2/auth',
-                'token_uri' => 'https://oauth2.googleapis.com/token',
-                'auth_provider_x509_cert_url' => 'https://www.googleapis.com/oauth2/v1/certs',
-                'client_x509_cert_url' => ''
-            ]
+            'projectId' => env('GOOGLE_CLOUD_PROJECT_ID'),
+            'keyFilePath' => env('GOOGLE_CLOUD_KEY_FILE'),
         ]);
 
         $bucket = $storage->bucket(env('GOOGLE_CLOUD_STORAGE_BUCKET'));

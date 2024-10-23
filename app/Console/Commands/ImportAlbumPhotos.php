ef236e2f16b7556400fd0eaf554f473eb4b49d16
albert <service0005@aibooksbank.com>
2024-10-23 09:56:30 +0800

    跑相簿

--- app/Console/Commands/ImportAlbumPhotos.php
+++ app/Console/Commands/ImportAlbumPhotos.php
@@ -44,16 +44,6 @@
                 // 抓取資料夾名稱作為套圖名稱
                 $albumName = basename($folder);
 
-                // 創建套圖
-                $album = Album::create([
-                                           'name' => $albumName,
-                                           'content' => '',
-                                           'cover_path' => '',
-                                           'actor_id' => $actor->id
-                                       ]);
-
-                $this->info('Album created: ' . $album->name);
-
                 // 掃描資料夾內的檔案
                 $files = File::files($folder);
                 $indexSort = 1;
@@ -61,26 +51,49 @@
                 // 只處理前5個檔案
                 $files = array_slice($files, 0, 5);
 
-                foreach ($files as $file) {
-                    $extension = strtolower($file->getExtension());
+                // 確保有檔案才創建套圖
+                if (!empty($files)) {
+                    // 抓取第一個檔案作為封面
+                    $firstFile = $files[0];
+                    $firstFileExtension = strtolower($firstFile->getExtension());
 
-                    // 僅抓取 jpg, jpeg, mp4, mov 檔案
-                    if (in_array($extension, ['jpg', 'jpeg', 'mp4', 'mov'])) {
-                        // 檔案路徑
-                        $relativePath = str_replace('/mnt/nas', '', $file->getPathname());
+                    // 只選取符合條件的檔案作為封面
+                    if (in_array($firstFileExtension, ['jpg', 'jpeg', 'mp4', 'mov'])) {
+                        $coverPath = str_replace('/mnt/nas', '', $firstFile->getPathname());
 
-                        // 將檔案插入 album_photos
-                        AlbumPhoto::create([
-                                               'album_id' => $album->id,
-                                               'photo_path' => $relativePath,
-                                               'index_sort' => $indexSort++
-                                           ]);
+                        // 創建套圖，並設定封面路徑
+                        $album = Album::create([
+                                                   'name' => $albumName,
+                                                   'content' => '',
+                                                   'cover_path' => $coverPath, // 設定封面路徑為第一筆檔案
+                                                   'actor_id' => $actor->id
+                                               ]);
 
-                        $this->info("File added: $relativePath");
+                        $this->info('Album created with cover: ' . $album->name);
+
+                        // 插入檔案資料
+                        foreach ($files as $file) {
+                            $extension = strtolower($file->getExtension());
+
+                            // 僅抓取 jpg, jpeg, mp4, mov 檔案
+                            if (in_array($extension, ['jpg', 'jpeg', 'mp4', 'mov'])) {
+                                // 檔案路徑
+                                $relativePath = str_replace('/mnt/nas', '', $file->getPathname());
+
+                                // 將檔案插入 album_photos
+                                AlbumPhoto::create([
+                                                       'album_id' => $album->id,
+                                                       'photo_path' => $relativePath,
+                                                       'index_sort' => $indexSort++
+                                                   ]);
+
+                                $this->info("File added: $relativePath");
+                            }
+                        }
                     }
                 }
             }
 
-            $this->info('Import completed successfully with a 5-record limit per album!');
+            $this->info('Import completed successfully with a 5-record limit per album and cover set as first file!');
         }
     }

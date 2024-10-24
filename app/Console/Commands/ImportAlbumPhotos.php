f16a2dbd5dfc49870823fd85d8c3a17569d2fef6
MSI\s454666 <s454666123@gmail.com>
2024-10-24 21:20:01 +0800

    更新路徑

--- app/Console/Commands/ImportAlbumPhotos.php
+++ app/Console/Commands/ImportAlbumPhotos.php
@@ -19,14 +19,14 @@
         public function handle()
         {
             // 固定演員名稱
-            $actorName = '猫九酱';
+            $actorName = '六味帝皇酱';
 
             // 如果演員不存在，則創建
             $actor = Actor::firstOrCreate(['actor_name' => $actorName], ['secondary_actor_name' => '']);
             $this->info('Actor found or created: ' . $actor->actor_name);
 
             // 目標資料夾
-            $targetDir = '/mnt/nas/b2/套圖專區/猫九酱';
+            $targetDir = '/mnt/nas/b2/套圖專區/六味帝皇酱';
 
             // 檢查資料夾是否存在
             if (!File::exists($targetDir)) {

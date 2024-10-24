1cee15304f186b6fcb5b477cbf3190509b72a9c4
MSI\s454666 <s454666123@gmail.com>
2024-10-24 23:17:35 +0800

    增加delete

--- app/Console/Commands/ImportAlbumPhotos.php
+++ app/Console/Commands/ImportAlbumPhotos.php
@@ -19,14 +19,14 @@
         public function handle()
         {
             // 固定演員名稱
-            $actorName = '爆机少女喵小吉';
+            $actorName = '橙子喵酱';
 
             // 如果演員不存在，則創建
             $actor = Actor::firstOrCreate(['actor_name' => $actorName], ['secondary_actor_name' => '']);
             $this->info('Actor found or created: ' . $actor->actor_name);
 
             // 目標資料夾
-            $targetDir = '/mnt/nas/b2/套圖專區/爆机少女喵小吉';
+            $targetDir = '/mnt/nas/b2/套圖專區/橙子喵酱';
 
             // 檢查資料夾是否存在
             if (!File::exists($targetDir)) {

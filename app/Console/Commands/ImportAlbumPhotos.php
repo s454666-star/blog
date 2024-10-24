469071183a10d458a76a8ebc061e6130ec6aaea1
MSI\s454666 <s454666123@gmail.com>
2024-10-24 23:47:58 +0800

    遞迴跑子資料夾

--- app/Console/Commands/ImportAlbumPhotos.php
+++ app/Console/Commands/ImportAlbumPhotos.php
@@ -19,7 +19,7 @@ class ImportAlbumPhotos extends Command
     public function handle()
     {
         // 固定演員名稱
-        $actorName = '猫九酱';
+        $actorName = '紧急企划';
 
         // 如果演員不存在，則創建
         $actor = Actor::firstOrCreate(
@@ -29,7 +29,7 @@ class ImportAlbumPhotos extends Command
         $this->info('找到或創建演員: ' . $actor->actor_name);
 
         // 目標資料夾
-        $targetDir = '/mnt/nas/b2/套圖專區/猫九酱';
+        $targetDir = '/mnt/nas/b2/套圖專區/紧急企划';
 
         // 檢查資料夾是否存在
         if (!File::exists($targetDir)) {

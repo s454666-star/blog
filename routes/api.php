d3b0f2add1ba100272e364b4fea39f00d408aa16
albert <service0005@aibooksbank.com>
2024-10-30 13:34:52 +0800

    會員api

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@
     use App\Http\Controllers\AlbumController;
     use App\Http\Controllers\AlbumPhotoController;
     use App\Http\Controllers\FileScreenshotController;
+    use App\Http\Controllers\MemberController;
     use App\Http\Controllers\ProductCategoryController;
     use App\Http\Controllers\ProductController;
     use App\Http\Controllers\UserController;

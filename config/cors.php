23a9b10906934dff35bea8daf2aa4e281637a610
s454666-star <s454666123@gmail.com>
2024-11-18 09:20:11 +0800

    跨域問題

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000','https://star-admin.mystar.monster/'],
+    'allowed_origins' => ['http://localhost:3000', 'https://star-admin.mystar.monster'],
 
     'allowed_origins_patterns' => [],
 

76654a0b132e31fbd0608e20a84ba7d40880d4c6
starwuwvt <s454666123@gmail.com>
2025-05-24 14:17:05 +0800

    TG機器人測試

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -50,7 +50,4 @@ class Kernel extends ConsoleKernel
         require base_path('routes/console.php');
     }
 
-    protected $commands = [
-        \App\Console\Commands\SetMyCommandsCommand::class,
-    ];
 }

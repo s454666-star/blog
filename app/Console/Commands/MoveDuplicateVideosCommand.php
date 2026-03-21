53b5910d28bd88d0f61d1eedec95a0bc8772aba5
starwuwvt <s454666123@gmail.com>
2026-03-21 09:55:53 +0800

    修正截圖

--- app/Console/Commands/MoveDuplicateVideosCommand.php
+++ app/Console/Commands/MoveDuplicateVideosCommand.php
@@ -20,6 +20,7 @@ class MoveDuplicateVideosCommand extends Command
      * php artisan video:move-duplicates "C:\Users\User\Pictures\train\downloads\group_2755698006_小荷才露尖尖角\videos\tmp" --video-feature-id=3323
      * php artisan video:move-duplicates "C:\Users\User\Videos\暫"
      * php artisan video:move-duplicates "C:\Users\User\Downloads\Video"
+     * php artisan video:move-duplicates "C:\Users\User\Downloads\Telegram Desktop"
      * php artisan video:move-duplicates "C:\incoming\a.mp4" --video-feature-id=123
      * php artisan video:move-duplicates "C:\incoming\a.mp4" --video-feature-id=123 --skip-log
      * php artisan video:move-duplicates "D:\incoming" --recursive=0 --threshold=92

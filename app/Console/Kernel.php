c67e47505898da2832ba9be7cdad7d6a151cd816
s454666-star <54258196+s454666-star@users.noreply.github.com>
2026-04-07 12:08:05 +0800

    fix time

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -41,7 +41,7 @@ class Kernel extends ConsoleKernel
             $restoreTargetBotUsername,
             $restoreWorkerEnvPath
         ))
-            ->dailyAt('11:46')
+            ->dailyAt('01:00')
             ->name('blog-filestore-restore-pending-sessions')
             ->withoutOverlapping(1440)
             ->runInBackground()

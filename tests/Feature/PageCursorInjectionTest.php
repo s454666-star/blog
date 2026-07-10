7f61c9d8f839f4a44e0d428c2b606e2eaee2c37c
s454666-star <54258196+s454666-star@users.noreply.github.com>
2026-07-10 15:07:00 +0800

    Replace fancy cursor with Kittydog Crystal set

--- tests/Feature/PageCursorInjectionTest.php
+++ tests/Feature/PageCursorInjectionTest.php
@@ -6,18 +6,18 @@ use Tests\TestCase;
 
 class PageCursorInjectionTest extends TestCase
 {
-    public function test_home_page_injects_fancy_cursor_assets(): void
+    public function test_web_portal_injects_kittydog_cursor_assets(): void
     {
-        $response = $this->get('/');
+        $response = $this->get('/web');
 
         $response->assertOk();
         $response->assertSee('fancy-cursor.css', false);
         $response->assertSee('fancy-cursor.js', false);
-        $response->assertSee('data-fancy-cursor="balloon"', false);
-        $response->assertSee('data-fancy-cursor-page="home"', false);
+        $response->assertSee('data-fancy-cursor=', false);
+        $response->assertSee('data-fancy-cursor-page="web"', false);
     }
 
-    public function test_command_runner_page_uses_magic_wand_cursor_theme(): void
+    public function test_command_runner_page_still_receives_custom_cursor_markup(): void
     {
         $response = $this->get('/command-runner');
 
@@ -43,5 +43,31 @@ class PageCursorInjectionTest extends TestCase
         $response->assertDontSee('fancy-cursor.css', false);
         $response->assertDontSee('fancy-cursor.js', false);
         $response->assertDontSee('data-fancy-cursor=', false);
+    }
+
+    public function test_explicitly_excluded_pages_keep_the_native_cursor(): void
+    {
+        foreach (['/folder-video-app', '/tw-stock/esun-portfolio', '/tw-stock/yuanta-portfolio'] as $path) {
+            $response = $this->get($path);
+
+            $response->assertDontSee('fancy-cursor.css', false);
+            $response->assertDontSee('fancy-cursor.js', false);
+            $response->assertDontSee('data-fancy-cursor=', false);
+        }
+    }
+
+    public function test_kittydog_cursor_styles_reference_the_downloaded_cursor_set(): void
+    {
+        $css = file_get_contents(public_path('css/fancy-cursor.css'));
+        $javascript = file_get_contents(public_path('js/fancy-cursor.js'));
+
+        $this->assertIsString($css);
+        $this->assertIsString($javascript);
+        $this->assertStringContainsString('/cursors/kittydog-crystal/normal.cur', $css);
+        $this->assertStringContainsString('/cursors/kittydog-crystal/link.cur', $css);
+        $this->assertStringContainsString('/cursors/kittydog-crystal/text.cur', $css);
+        $this->assertStringContainsString('/cursors/kittydog-crystal/unavailable.cur', $css);
+        $this->assertStringContainsString('kittydog-cursor-enabled', $javascript);
+        $this->assertStringNotContainsString('fancy-cursor-layer', $javascript);
     }
 }

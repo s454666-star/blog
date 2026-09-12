2b2145460f24a3ffc4a0b0e34016c9c4d057f547
s454666-star <54258196+s454666-star@users.noreply.github.com>
2026-09-12 16:40:01 +0800

    Serve stock dashboard assets from the primary static origin

--- resources/views/tw-stock/partials/experience-head.blade.php
+++ resources/views/tw-stock/partials/experience-head.blade.php
@@ -1,3 +1,9 @@
 {{-- Opt-in presentation only. Never include on the futures chart or its home alias. --}}
-<link rel="stylesheet" href="{{ asset('css/tw-stock-experience.css') }}?v=20260912-1">
-<script src="{{ asset('js/tw-stock-experience.js') }}?v=20260912-1" defer></script>
+@php
+    // The stock subdomain forwards application pages; static files live on the primary origin.
+    $stockExperienceAssetRoot = request()->getHost() === 'stock.mystar.monster'
+        ? 'https://mystar.monster/'
+        : rtrim(asset(''), '/') . '/';
+@endphp
+<link rel="stylesheet" href="{{ $stockExperienceAssetRoot }}css/tw-stock-experience.css?v=20260912-1">
+<script src="{{ $stockExperienceAssetRoot }}js/tw-stock-experience.js?v=20260912-1" defer></script>
